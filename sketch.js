e3f591be94f6c0d8be5307ea28fc5f518bf24c83
arjunsuman11 <anyway.arjun@gmail.com>
2021-01-09 12:58:22 +0530

    DBStage1

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
-var ball;
+var ball, db, position;
 
 function setup(){
     createCanvas(500,500);
+
+    db = firebase.database();
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var locBall = db.ref("Ball/Position");
+    locBall.on("value", readPos, showErr);
+
 }
 
 function draw(){
@@ -23,7 +30,36 @@ function draw(){
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function changePosition(X,Y){
+    db.ref("Ball/Position").set({
+        x:position.x+X,
+        y:position.y+Y
+    });
 }
+
+function readPos(data){
+    position = data.val();  //copies everything from data to position
+
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
+
+function showErr(){
+    console.log("ERROR!!!")
+}
+
+/*
+.ref() - refers to the location of value that we want
+
+READ
+.on() - turns on a listener which listens to new changes in the field
+        - 2 functions 
+            1. Reads the value - necessary
+            2. Tells us error
+
+WRITE
+.set() - updates/writes that field
+
+
+*/
